9f796dd44adc43f33ce7809e38dd4fa5b2298da1
gustavo.peiretti <contacto@gustavopeiretti.com>
2022-08-22 15:46:10 -0300

    Spring Boot Database Init SQL Files

---
+++ spring-boot-database-init-sql-files/src/main/java/com/gustavopeiretti/springbootcourse/model/User.java
@@ -0,0 +1,57 @@
+package com.gustavopeiretti.springbootcourse.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String surname;
+    private LocalDate birthDate;
+
+    public User() {
+    }
+
+    // getters.. setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+}
