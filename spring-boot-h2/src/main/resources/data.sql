b73af8dee4b507a3df722be2008028fd7ea0634c
gustavo.peiretti <contacto@gustavopeiretti.com>
2022-08-22 16:12:12 -0300

    Spring Boot H2 Config

---
+++ spring-boot-h2/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (id, birth_date, name, surname)
+VALUES (1, '1960-06-10', 'Paul', 'Hewson');
+INSERT INTO user (id, birth_date, name, surname)
+VALUES (2, '1960-03-13', 'Adam', 'Clayton');
+INSERT INTO user (id, birth_date, name, surname)
+VALUES (3, '1961-08-08', 'David', 'Evans');
