6dfaccf5e90f58c481937bf4d35c348a4d51988b
gustavo.peiretti <contacto@gustavopeiretti.com>
2023-03-07 15:18:09 -0300

    Spring Boot Autowired

---
+++ spring-boot-autowired/readme.md
@@ -0,0 +1,5 @@
+# Understanding Spring Boot @Autowired Annotation
+
+Example code for [spring-boot-autowired-annotation/](https://gustavopeiretti.com/spring-boot-autowired-annotation/)
+
+
---
+++ spring-boot-autowired/src/test/java/com/gustavopeiretti/springbootautowired/SpringBootAutowiredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavopeiretti.springbootautowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutowiredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
