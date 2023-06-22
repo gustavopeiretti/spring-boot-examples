8db8394c1f546a4c7bd4aa9829829e3c056a2cd1
gustavo.peiretti <contacto@gustavopeiretti.com>
2023-06-22 17:41:09 -0300

    Spring Boot Docker

---
+++ spring-boot-maven-wrapper/src/test/java/com/gustavopeiretti/springbootmavenwrapper/SpringBootMavenWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavopeiretti.springbootmavenwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenWrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
